49b3a86a3582affbc481ae169cc8305eca646351
liming <827182486@qq.com>
2020-07-13 22:33:25 +0800

    Shell上下文

--- microblog.py
+++ microblog.py
@@ -4,4 +4,10 @@
 # @Author  : 一叶知秋
 # @File    : microblog.py
 # @Software: PyCharm
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
