3509ff30d6d9deda9cc24d22e805f8ef1bc39e00
liming <827182486@qq.com>
2020-07-13 20:42:20 +0800

    "Hello, World" Flask应用

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,6 @@
 # @Software: PyCharm
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,23 @@
 # @Author  : 一叶知秋
 # @File    : routes.py
 # @Software: PyCharm
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    # return 'Hello, World!'
+    user = {'username': 'miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
