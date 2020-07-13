e2a799a39477f55949d01d3759b9cfa63045b547
liming <827182486@qq.com>
2020-07-13 21:52:20 +0800

    配置数据库

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,12 @@
 # @Software: PyCharm
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes,models
--- config.py
+++ config.py
@@ -6,6 +6,11 @@
 # @Software: PyCharm
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
