500903dd8a67db8a1c4e2af2a25c953b725a021b
liming <827182486@qq.com>
2020-07-14 15:58:53 +0800

    在模板中显示已登录的用户

--- app/routes.py
+++ app/routes.py
@@ -14,10 +14,9 @@ from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
-@login_required
 def index():
     # return 'Hello, World!'
-    user = {'username': 'miguel'}
+    # user = {'username': 'miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -28,7 +27,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -46,11 +45,22 @@ def login():
         print(next_page)
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return redirect(url_for(next_page))
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(email=form.email.data).first()
+#         if user is not None and user.verify_password(form.password.data):
+#             login_user(user, form.remember_me.data)
+#             return redirect(request.args.get('next') or url_for('index'))
+#         flash('Invalid username or password.')
+#     return render_template('login.html', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
+    flash ('You have been logged out')
     return redirect(url_for('index'))
