d1a916c2547d651d7d1731c0090fdc222fc83c0b
liming <827182486@qq.com>
2020-07-14 13:35:32 +0800

    用户登入

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
+
+login = LoginManager(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes,models
+from app import routes, models
