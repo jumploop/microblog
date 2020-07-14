7a33412e8de745b8fe10786d05a15fb7aef88397
liming <827182486@qq.com>
2020-07-14 14:25:06 +0800

    要求用户登录

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 
 login = LoginManager(app)
+login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
